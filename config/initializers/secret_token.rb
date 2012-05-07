e645d6cd266108018955be34ceecbc6ceab23033
Andrew Olson <anolson@gmail.com>
2012-05-07 16:26:41 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ViddlerIo::Application.config.secret_token = 'cb5156374fa990ab636b4fa2c6579aa555da5f60e2c5405260cc5d197e1ff11d52f1399b7a0840d20c7595fd95c6fbd22e0729b28ee9311811f3402e5cb6ed15'
