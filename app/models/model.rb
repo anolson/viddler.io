5102ed5d3cd9a4426c637bc7c8690c3b15a8e8ba
Andrew Olson <anolson@gmail.com>
2012-05-10 17:01:36 -0400

    Pulling client method back into Model.

--- app/models/model.rb
+++ app/models/model.rb
@@ -5,14 +5,22 @@ class Model
   include ActiveModel::Validations
   include ActiveModel::Conversion
   extend ActiveModel::Naming
-  
+
   def initialize(attributes = {})
     attributes.each { |name, value|
       send "#{name}=", value
     }
   end
-  
+
   def persisted?
     false
+  end
+
+  def client
+    @client ||= self.class.client
+  end
+
+  def self.client
+    Viddler::Client.new('1bo1odc4du4nmcp1qx4w')
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,12 @@
 class User < Model
   attr_accessor :username, :password
-  
+
   validates_presence_of :username, :password
-  
+
   def authenticate!(session)
     if valid?
       client.authenticate!(@username, @password)
       session[:viddler_session_id] = client.sessionid if client.authenticated?
     end
   end
-  
-  private 
-  
-  def client
-    @client ||= Viddler::Client.new('1bo1odc4du4nmcp1qx4w')
-  end
-
 end
